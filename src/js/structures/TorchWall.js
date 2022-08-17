c79fad0142089c529097c201bb2c9f05157a6912
Riley Cleary <rileykeycleary@yahoo.com>
2022-08-17 16:24:23 -0600

    Fixes torch wall rendering, re builds main.js

--- src/js/structures/TorchWall.js
+++ src/js/structures/TorchWall.js
@@ -7,12 +7,11 @@ import {UP, LEFT, RIGHT, TS} from "../constants"
 class TorchWall extends Structure{
     constructor(x, y, direction){
         super(x,y);
-        this.torchImage = new Image();
         this.direction = direction;
         this.sprite = sprite({
             width: 64,
             height: 16,
-            image: this.torchImage,
+            image: imgs.torch,
             numberOfFrames: 4,
             sizescale: .065,
             defaultSize:true
@@ -23,21 +22,21 @@ class TorchWall extends Structure{
     build(){
         switch(this.direction){
             case UP:
-                this.torchImage = imgs.torch;
+                this.sprite.image = imgs.torch;
                 this.sprite.x = this.x*TS;
                 this.sprite.y = (this.y+1)*TS;
                 this.selfArray.push(new WallTile2(imgs.wallMidTop, 3 , this.x, this.y, {}));
                 this.selfArray.push(new WallTile2(imgs.wallMid, 1 , this.x, this.y+1, {obstructing:true}));
                 break;
             case LEFT:
-                this.torchImage = imgs.torchSideLeft;
+                this.sprite.image = imgs.torchSideLeft;
                 this.sprite.x = this.x*TS;
                 this.sprite.y = this.y*TS;
                 this.selfArray.push(new WallTile2(imgs.wallSideMidRight, 3 , this.x, this.y, {obstructing:true, hitboxLeft:true}));
                 this.selfArray.push(new FloorTile(this.x, this.y));
                 break;
             case RIGHT:
-                this.torchImage = imgs.torchSideRight;
+                this.sprite.image = imgs.torchSideRight;
                 this.sprite.x = this.x*TS;
                 this.sprite.y = this.y*TS;
                 this.selfArray.push(new WallTile2(imgs.wallSideMidLeft, 3 , this.x, this.y, {obstructing:true, hitboxRight:true}));
